979f5e129c484d22480aac468ef42acf2912840b
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-01-29 13:43:13 -0300

    :recycle: refactor: Adição da logica de upload de imagem

--- src/main/java/com/example/minioupdateimages/controller/UpdateImageController.java
+++ src/main/java/com/example/minioupdateimages/controller/UpdateImageController.java
@@ -1,4 +1,38 @@
 package com.example.minioupdateimages.controller;
 
+import com.example.minioupdateimages.service.UploadImageService;
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("images")
 public class UpdateImageController {
+
+    private final UploadImageService uploadImageService;
+    private final MinioClient minioClient;
+
+    public UpdateImageController(UploadImageService uploadImageService, MinioClient minioClient) {
+        this.uploadImageService = uploadImageService;
+        this.minioClient = minioClient;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> execute(@RequestParam MultipartFile file) throws Exception {
+        uploadImageService.uploadImage(file);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/{objectId}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getImage(@PathVariable String objectId) throws Exception {
+        var stream = minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket("images").object(objectId)
+                        .build());
+                return IOUtils.toByteArray(stream);
+    }
 }
--- src/main/java/com/example/minioupdateimages/service/UploadImageService.java
+++ src/main/java/com/example/minioupdateimages/service/UploadImageService.java
@@ -1,4 +1,39 @@
 package com.example.minioupdateimages.service;
 
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
 public class UploadImageService {
+
+    private final MinioClient minioClient;
+    private final JdbcClient jdbcClient;
+
+    public UploadImageService(MinioClient minioClient, JdbcClient jdbcClient) {
+        this.minioClient = minioClient;
+        this.jdbcClient = jdbcClient;
+    }
+
+    public void uploadImage(MultipartFile file) throws Exception {
+        var inputStream = file.getInputStream();
+        var objectId = UUID.randomUUID().toString();
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket("images")
+                        .object(objectId)
+                        .stream(inputStream, inputStream.available(), -1)
+                        .contentType("image/png")
+                        .build());
+
+        jdbcClient.sql("""
+                        INSERT INTO images (object_id) VALUES (:objectId)
+                        """)
+                .param(objectId)
+                .update();
+    }
 }
